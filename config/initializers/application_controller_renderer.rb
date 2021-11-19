070b480313f498b1c3a640ba190bdd527c22cb4e
takazawa1204 <ckicken1204@gmail.com>
2021-11-19 01:09:53 +0000

    3章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '66dfa04ca424241970d96ca3bddf475e580b7a45d9133d21c3c1c19fcce19fe29b1d9f1f0ac4fde694a5e6a8fba3a1978071b73a6d5f07472e4df04c46c1134f'
